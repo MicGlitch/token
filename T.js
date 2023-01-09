06412d9bf516db9166368468668a5fa0937e411d
MicGlitch <117761462+MicGlitch@users.noreply.github.com>
2023-01-10 00:00:21 +0530

    Update T.js

--- T.js
+++ T.js
@@ -5,14 +5,8 @@ const access_token = process.argv[2];
 
 const getAccessToken = async (id) => {
     let device_id = createRandomId();
-    let res = await fetch('https://api.kamyroll.tech/auth/v1/token', {
-        method: 'POST',
-        body: new URLSearchParams({
-            'device_id': device_id,
-            'device_type': 'com.something.crunchyroll',
-            'access_token': access_token
-        })
-    }).then((res) => res.json())
+    let res = await fetch(`https://api.kamyroll.tech/auth/v1/token?device_id=${device_id}&device_type=com.something.crunchyroll&access_token=${access_token}`)
+        .then((res) => res.json())
 
     fs.writeFileSync("welp.json", JSON.stringify(res))
 };
